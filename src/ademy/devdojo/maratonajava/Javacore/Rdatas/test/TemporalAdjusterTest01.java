93d02d7190412f9d09d3ac25b849106e9720005a
Initel <diegodidi2005@gmail.com>
2021-12-22 23:02:20 -0300

    Datas 5

--- src/ademy/devdojo/maratonajava/Javacore/Rdatas/test/TemporalAdjusterTest01.java
+++ src/ademy/devdojo/maratonajava/Javacore/Rdatas/test/TemporalAdjusterTest01.java
@@ -1,5 +1,7 @@
 package ademy.devdojo.maratonajava.Javacore.Rdatas.test;
 
+import ademy.devdojo.maratonajava.Javacore.Gassociacao.domain.Local;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.ChronoField;
@@ -12,28 +14,15 @@ class obterProximoDiaUtil implements TemporalAdjuster {
     @Override
     public Temporal adjustInto(Temporal temporal) {
         DayOfWeek dayOfWeek = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
-        int addDias = 0;
-        switch (dayOfWeek){
-            case THURSDAY:
-                addDias = 4;
-                break;
-
-            case FRIDAY:
-                addDias = 3;
-                break;
-                
-            case SATURDAY:
-                addDias = 2;
-                break;
-                
-            default:
-                addDias = 1;
-                break;
+        int addDays = 0;
+        switch (dayOfWeek) {
+            case THURSDAY: addDays = 4; break;
+            case FRIDAY: addDays = 3; break;
+            case SATURDAY: addDays = 2; break;
+            default: addDays = 1; break;
         }
-        return temporal.plus(addDias, ChronoUnit.DAYS);
+        return temporal.plus(addDays, ChronoUnit.DAYS);
     }
-
-
 }
 
 public class TemporalAdjusterTest01 {
@@ -41,8 +30,28 @@ public class TemporalAdjusterTest01 {
         LocalDate now = LocalDate.now();
         System.out.println(now);
         System.out.println(now.getDayOfWeek());
-        
-        now = LocalDate.now.with(new obterProximoDiaUtil());
+
+        System.out.println();
+
+        now = LocalDate.now().with(new obterProximoDiaUtil());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        System.out.println();
+
+        now = LocalDate.now().withDayOfMonth(24).with(new obterProximoDiaUtil());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        System.out.println();
+
+        now = LocalDate.now().withDayOfMonth(25).with(new obterProximoDiaUtil());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        System.out.println();
+
+        now = LocalDate.now().withDayOfMonth(26).with(new obterProximoDiaUtil());
         System.out.println(now);
         System.out.println(now.getDayOfWeek());
     }
